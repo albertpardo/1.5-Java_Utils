2d5d7ef6ef2adc8db818adbea86c13e2ece01d13
apm <apm@mail.com>
2025-11-18 13:13:24 +0100

    [T5L1E2] first aproach to detect Directory, File and get last modification"

--- src/level1ex2/ShowSortedFilesFromAGivenPath.java
+++ src/level1ex2/ShowSortedFilesFromAGivenPath.java
@@ -1,6 +1,9 @@
 package level1ex2;
 
 import java.io.File;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -9,6 +12,27 @@ public class ShowSortedFilesFromAGivenPath {
     static final String MSG_NO_EXIST = "Path not Exist!!";
     static final String MSG_NO_DIR = "This is not a directory!!";
     static final String MSG_SHOW_FILE_LIST = "** Files are:";
+
+    private static void printAsDirOrFile(String pathname, String fileName){
+        String newPathName = pathname + fileName;
+        String typeFile = "";
+
+        try {
+            File fileObject = new File(pathname);
+            if (fileObject.isDirectory())
+                typeFile = "(D)";
+            if (fileObject.isFile())
+                typeFile = "(F)";
+            long lastModified = fileObject.lastModified();
+
+            Instant instant = Instant.ofEpochMilli(lastModified);
+            LocalDateTime date = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+            System.out.println(fileName + " " +  typeFile + " " + date);
+        }
+        catch (Exception e) {
+        System.err.println(e.getMessage());
+        }
+    }
 
     public static void showSortedFiles(String pathname){
         try {
@@ -29,8 +53,8 @@ public class ShowSortedFilesFromAGivenPath {
                 filesArrayList = new ArrayList<>(Arrays.asList(filesString));
                 Collections.sort(filesArrayList);
                 System.out.println(MSG_SHOW_FILE_LIST);
-                for (String file : filesArrayList) {
-                    System.out.println(file);
+                for (String fileName : filesArrayList) {
+                    printAsDirOrFile(pathname, fileName);
                 }
             }
         }
