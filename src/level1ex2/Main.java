41ea673bd698f62e11b6135676e4816d7746099c
apm <apm@mail.com>
2025-11-18 11:48:23 +0100

    [T5L1E2] Start with code from [T5L1E1]

---
+++ src/level1ex2/Main.java
@@ -0,0 +1,21 @@
+package level1ex2;
+
+public class Main{
+
+    public static void main(String[] args){
+
+        switch (args.length) {
+            case 1:
+                ShowSortedFilesFromAGivenPath.showSortedFiles(args[0]);
+                break;
+            case 0:
+                System.err.println("No path in arguments!!");
+                break;
+            default:
+                System.err.println("Many arguments are not allowed!!");
+                break;
+        }
+    }
+
+
+}
---
+++ src/level1ex2/ShowSortedFilesFromAGivenPath.java
@@ -0,0 +1,41 @@
+package level1ex2;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ShowSortedFilesFromAGivenPath {
+    static final String MSG_NO_EXIST = "Path not Exist!!";
+    static final String MSG_NO_DIR = "This is not a directory!!";
+    static final String MSG_SHOW_FILE_LIST = "** Files are:";
+
+    public static void showSortedFiles(String pathname){
+        try {
+            File fileObject = new File(pathname);
+            String[] filesString;
+            ArrayList<String> filesArrayList;
+
+            if (!fileObject.exists()){
+                System.err.println(MSG_NO_EXIST);
+                return;
+            }
+            if (!fileObject.isDirectory()){
+                System.err.println(MSG_NO_DIR);
+                return;
+            }
+            filesString = fileObject.list();
+            if (filesString != null) {
+                filesArrayList = new ArrayList<>(Arrays.asList(filesString));
+                Collections.sort(filesArrayList);
+                System.out.println(MSG_SHOW_FILE_LIST);
+                for (String file : filesArrayList) {
+                    System.out.println(file);
+                }
+            }
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
